fa475e623820ca6c657676a97c6db52d33ee3d8e
Tanner Baldus <tanner.baldus@hpe.com>
2017-06-26 17:26:03 -0700

    Article DNE exception

--- exceptions/exceptions.py
+++ exceptions/exceptions.py
@@ -5,7 +5,14 @@ class LinkedArticleNotFound(RuntimeError):
         super().__init__(slug)
 
 
+class ArticleDoesNotExist(RuntimeError):
+    def __init__(self, slug):
+        self.slug = slug
+        super().__init__(slug)
+
+
 class CollectionNotSetup(RuntimeError):
     def __init__(self, collection_name):
         self.collection_name = collection_name
         super().__init__(collection_name)
+
