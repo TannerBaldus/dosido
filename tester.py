2e3c21c50ec3618bbc5b0f833b35f3d97ddd4ab1
Tanner Baldus <tanner.baldus@hpe.com>
2017-06-28 13:42:50 -0700

    Move filearticle to orm

--- tester.py
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-from api.client import ApiClient
-api_key = "63bd9b2c7c4e8bf66130e7d64815cfb90affb780"
-a = ApiClient(api_key)
-f =  open("pill/foo.md")
-r = a.upload_article(f, "5939de0904286305c68cf287", None)
