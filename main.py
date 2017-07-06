945bbaed38400d614315ccb6bf12e1c788c741d5
Tanner Baldus <tanner.baldus@hpe.com>
2017-07-06 14:51:06 -0700

    Pull out config setup into seperate file

--- main.py
+++ main.py
@@ -29,33 +29,15 @@ from docopt import docopt
 from distutils.util import strtobool
 
 from api.client import ApiClient
-from FileArticle import FileArticle
+from config import DosidoConfig, config_setup
+from helpscout_orm import Article, Collection
 from exceptions import *
 
 
-
-def query_user(query, yes_no=False):
-    if yes_no:
-        answer = None
-        while answer is None:
-            try:
-                answer = bool(strtobool(input("{} (yes/no)\n".format(query))))
-            except ValueError:
-                answer = bool(strtobool(input("Please answer yes, y, n or no.\n")))
-        return answer
-    return input("{}\n".format(query))
-
-
 class Dosido(object):
-
-    def __init__(self):
-        self.config_filename = "config.ini"
-        self.config = configparser.ConfigParser()
-        self.config.read(self.config_filename)
-        self.api_key = (self.config.has_option("api_info", "api_key") and self.config.get("api_info", "api_key")) or None
-        self.api_client = None if not self.api_key else ApiClient(self.api_key)
-
-    def _get_md_files(self, file_pattern):
+    
+    @classmethod
+    def _get_md_files(cls, file_pattern):
         file_paths = [i for i in glob(file_pattern) if i.endswith(".md")]
         if not file_paths:
             raise ValueError("No md files found")
@@ -82,41 +64,10 @@ class Dosido(object):
 
         print("Done")
 
-    def initialize(self):
-        api_key = query_user("What is your api key?")
-        self.config.set("api_info", "api_key",  api_key)
-        self.api_client = ApiClient(api_key)
+    @classmethod
+    def initialize(cls):
+        config_setup.initialize()
 
-        site_id = self.setup_site()
-        self.config.set("api_info", "site_id", str(site_id))
-        collections = self.setup_collections(site_id)
-        for name, collection_id in collections.items():
-            self.config.set("collections", name, collection_id)
-        self.config.write(open(self.config_filename, "w"))
-
-    def setup_site(self):
-        has_site = query_user("Do you have already have a knowledge base site?", yes_no=True)
-        if has_site:
-            sub_domain = query_user("What is the sub domain?")
-            site = self.api_client.get_site_by_sub_domain(sub_domain)
-        else:
-            sub_domain = query_user("Provide a sub domain for your site. Url will be subdomain.helpscoutdocs.com?")
-            title = query_user("Provide a title for your site.")
-            site = self.api_client.create_site(sub_domain, title)
-        return site["id"]
-
-    def setup_collections(self, site_id):
-        another = True
-        collections = {}
-
-        while another:
-            name = query_user("Provide a name for the collection.")
-            private = query_user("Should this collection be private? Default is public.", yes_no=True)
-            collection = self.new_collection(name, site_id, private)
-            collections[name] = collection["id"]
-            os.makedirs(name)
-            another = query_user("Would you like to make another collection?", yes_no=True)
-        return collections
 
     def new_collection(self, name, site_id, private, no_dir):
         visibility = "private" if private else "public"
