87c2f95e7085ec241821e57afbcbb2e63122ce2d
Tanner Baldus <tanner.baldus@hpe.com>
2017-07-06 14:41:42 -0700

    Just wrap configparser and don't inherit

--- config/dosido_config.py
+++ config/dosido_config.py
@@ -6,19 +6,19 @@ import os
 from .constants import CONFIG_FILEPATH
 
 
-class DosidoConfig(ConfigParser):
+class DosidoConfig():
     settings_path = CONFIG_FILEPATH
 
     def __init__(self):
-        super(DosidoConfig).__init__()
-        self.read(self.config_filepath)
-        if not self.sections():
+        self.config_parser = ConfigParser()
+        self.config_parser.read(self.config_filepath)
+        if not self.config_parser.sections():
             self._initialize_sections()
 
     def _initialize_sections(self):
-        self.add_section("api_info")
-        self.add_section("collections")
-        self.add_section("site_details")
+        self.config_parser.add_section("api_info")
+        self.config_parser.add_section("collections")
+        self.config_parser.add_section("site_details")
 
     @property
     def config_filepath(self):
@@ -26,37 +26,37 @@ class DosidoConfig(ConfigParser):
         for dir_path in Path(os.getcwd()).parents:
             if config_path:
                 return config_path[0]
-            config_path = glob(os.path.join(dir_path, self.settings_path))
+            config_path = glob(os.path.join(dir_path, self.config_parser.settings_path))
         raise ValueError("no path")
 
     def get_collection(self, collection_name):
-        return self.get("collections", collection_name)
+        return self.config_parser.get("collections", collection_name)
 
     def add_collection(self, collection_name, collection_id):
-        self.set("collections", collection_name, collection_id)
+        self.config_parser.set("collections", collection_name, collection_id)
 
     @property
     def api_key(self):
-        return self.get("api_info", "api_key")
+        return self.config_parser.get("api_info", "api_key")
 
     @api_key.setter
     def api_key(self, api_key_str):
-        self.set("api_info", "api_key", api_key_str)
+        self.config_parser.set("api_info", "api_key", api_key_str)
 
     @property
     def site_id(self):
-        return self.get("site_details", "site_id")
+        return self.config_parser.get("site_details", "site_id")
 
     @site_id.setter
     def site_id(self, site_id):
-        self.set("site_details", "site_id", site_id)
+        self.config_parser.set("site_details", "site_id", site_id)
 
     @property
     def image_host(self):
-        return self.get("site_details", "image_host")
+        return self.config_parser.get("site_details", "image_host")
 
     @image_host.setter
     def image_host(self, image_host_url):
-        self.set("site_details", "image_host", image_host_url)
+        self.config_parser.set("site_details", "image_host", image_host_url)
 
 
