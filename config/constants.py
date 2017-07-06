354dc35013dd51a52bcb38ccb3331e9253c05f6f
Tanner Baldus <tanner.baldus@hpe.com>
2017-07-06 14:42:14 -0700

    Join filepath correctly

--- config/constants.py
+++ config/constants.py
@@ -2,4 +2,4 @@ import os
 
 CONFIG_FILENAME = "config.ini"
 CONFIG_DIR = ".dosido"
-CONFIG_FILEPATH = os.path.join(CONFIG_FILENAME, CONFIG_DIR)
+CONFIG_FILEPATH = os.path.join(CONFIG_DIR, CONFIG_FILENAME)
