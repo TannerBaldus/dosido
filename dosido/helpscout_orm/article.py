f4672331b352b9634c802667990edf41d80e6fe4
Tanner Baldus <tanner.baldus@hpe.com>
2017-08-29 16:30:56 -0700

    Use search by title in article

--- dosido/helpscout_orm/article.py
+++ dosido/helpscout_orm/article.py
@@ -14,19 +14,18 @@ class Article(BaseObject):
     def __init__(self, file_path, config):
         super().__init__(config)
         self.file_path = file_path
-        self.slug = self._path_to_slug(file_path)
         self.title = ' '.join(Path(file_path).stem.split("_"))
         self.collection = Collection(self.config, self.collection_name_from_path(file_path))
 
     def create(self, skip_article_refs, publish):
         status = "published" if publish else "notpublished"
         if self._article_id:
-            raise ArticleAlreadyExists(self.slug)
-        return self.api_client.create_article(self.collection.id, self.title, self.convert_text(skip_article_refs), slug=self.slug, status=status)
+            raise ArticleAlreadyExists(self.title)
+        return self.api_client.create_article(self.collection.id, self.title, self.convert_text(skip_article_refs), status=status)
 
     def update(self, is_draft, skip_article_refs, unpublish):
         if not self._article_id:
-            raise ArticleDoesNotExist(self.slug)
+            raise ArticleDoesNotExist(self.title)
         if is_draft:
             return self.api_client.save_draft(self._article_id, self.convert_text(skip_article_refs))
         status = "notpublished" if unpublish else None
@@ -34,7 +33,7 @@ class Article(BaseObject):
                                               name=self.title, status=status)
     @property
     def _article_id(self):
-        article_response = self.api_client.get_article_by_slug(self.slug, collection_id=self.collection.id)
+        article_response = self.api_client.get_article_by_title(self.title, collection_id=self.collection.id)
         return None if not article_response else article_response["id"]
 
     @staticmethod
@@ -116,9 +115,9 @@ class Article(BaseObject):
 
     def _get_public_url_from_path(self, article_path):
         linked_article = Article(article_path, self.config)
-        article_response = self.api_client.get_article_by_slug(linked_article.slug)
+        article_response = self.api_client.get_article_by_title(linked_article.title)
         if not article_response:
-            raise LinkedArticleNotFound(linked_article.slug, linked_article.collection)
+            raise LinkedArticleNotFound(linked_article.title, linked_article.collection)
         return article_response["url"]
 
     def _convert_asset_link(self, tag_url):
